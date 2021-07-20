e47e43a68ba814be1dca817283aeaedc69b2d8cb
Johnny Barragan <johnhbarragan@gmail.com>
2021-07-20 00:12:56 -0700

    updated aboutme lab 3

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 'use strict'
 
+let correctguesses = 0;
 let username = prompt('What\'s your name?');
 
 alert('Hello ' + username + '. Glad to have you here.');
 
+//Question 1
 let answerTime = prompt('Do you have time to take a short quiz about me? Answer \'Yes\'\ or \'No\'').toLowerCase();
 
 //console.log('Do they have time to answer questions ' + answerTime);
 if (answerTime === 'yes' || answerTime === 'y'){
     alert('Thank you for answering Yes, let\'s get started!');
+    correctguesses++;
 
 } else if (answerTime === 'no' || answerTime === 'n'){
     alert('Since you don\'t have time, please come back another time!');
@@ -16,22 +19,27 @@ if (answerTime === 'yes' || answerTime === 'y'){
     alert('Please answer with Yes or No');
 }
 
+//Question 2
 let answerOne = prompt('Am I from Pasco, WA? Answer \'Yes\'\ or \'No\'').toLowerCase();
 
 //console.log('Do they know where I\'m from?: ' + answerOne);
 if (answerOne === 'yes' || answerOne === 'y'){
     alert('Yes! I am from Pasco, WA');
+    correctguesses++;
 
 } else if (answerOne === 'no' || answerOne === 'n'){
     alert('It\'s true...I was born and raised in Pasco, WA');
 } else {
     alert('Please answer with Yes or No')
 }
+
+//Question 3
 let answerFamily = prompt('Am I the youngest of 5 kids? Answer \'Yes\'\ or \'No\'').toLowerCase();
 
 //console.log('Do they know if I'm youngest in family?: ' + answerFamily);
 if (answerFamily === 'yes' || answerFamily === 'y'){
     alert('You are correct!');
+    correctguesses++;
 
 } else if (answerFamily === 'no' || answerFamily === 'n'){
     alert('It\'s true, I am the youngest!');
@@ -39,11 +47,13 @@ if (answerFamily === 'yes' || answerFamily === 'y'){
     alert('Please answer with Yes or No');
 }
 
+//Question 4
 let answerFood = prompt('Are cheesburgers my favorite food? Answer \'Yes\'\ or \'No\'').toLowerCase();
 
 //console.log('Do they know my favorite food: ' + answerFood);
 if (answerFood === 'yes' || answerFood === 'y'){
     alert('You are correct! Who doesn\'t love a cheeseburger?');
+    correctguesses++;
 
 } else if (answerFood === 'no' || answerFood === 'n'){
     alert('You are incorrect, I love me a big cheesy cheesburger');
@@ -51,11 +61,13 @@ if (answerFood === 'yes' || answerFood === 'y'){
     alert('Please answer with Yes or No');
 }
 
+//Question 5
 let answerFriend = prompt('Do you want to be friends? Answer \'Yes\'\ or \'No\'').toLowerCase();
 
 //console.log('Do they want to be friends: ' + answerFriend);
 if (answerFriend === 'yes' || answerFriend === 'y'){
     alert('Thanks for saying Yes! I love making new friends!');
+    correctguesses++;
 
 } else if (answerFriend === 'no' || answerFriend === 'n'){
     alert('Since you said No, go away!');
@@ -63,16 +75,21 @@ if (answerFriend === 'yes' || answerFriend === 'y'){
     alert('Please answer with Yes or No');
 }
 
+//Question 6
 let johnnyNephews = 7;
 let attempts = 4;
  
-while (attempts){
-for (let i=0; i < attempts; i+1) {
-    let answerNephews = +prompt ('Can you guess how many nephews I have?');{
-        console.log (`Guess of Johnny's nephews: ${answerNephews}`);
+while (attempts) {
+    for (let i=0; i < 4; i++) {
+    
+    let answerNephews = +prompt ('Can you guess how many nephews I have?'); {
+        console.log (`How many nephews do they think I have: ${answerNephews}`);
+    }
 
-  } if (answerNephews === johnnyNephews) {
+    if (answerNephews === johnnyNephews) {
+      attempts = 0;
       alert ('You got it!');
+      correctguesses++;
       break;
 
   } else if (answerNephews < johnnyNephews) {
@@ -83,40 +100,44 @@ for (let i=0; i < attempts; i+1) {
     attempts--; 
     alert (`You are too high! You have ${attempts} guesses left`);
     
-    } if (attempts === 0) {
+    } if (attempts === 0 || answerNephews !== johnnyNephews) {
       alert (`Since there are no guess left, I have 7 nephews!`);
   } 
 }
 
+
+
+//Question 7
 let johnnyCandy = ['snickers', 'kit kat', 'skittles'];
-let attemptsCandy = 5;
+let attemptsCandy = 6;
 
 while (attemptsCandy) {
+    for (let i = 0; i < 6; i++) {
+    
     let answerCandy = prompt('What are my favorite candy?'); {
         console.log(`Guess of Johnny's favorite candy: ${answerCandy}`);
     }
-    for (let i = 0; i < attemptsCandy; i+1) {
+    
+    
     
     if (answerCandy === johnnyCandy[i]){
     attemptsCandy = 0;
     alert('You are correct!');
+    correctguesses++;
     break;
     }
-
-    else if (answerCandy !== johnnyCandy[i]){ 
+    
+    else if (answerCandy !== johnnyCandy[i]){
+    attemptsCandy--; 
     alert(`That is not one of my favorite candies. You have ${attemptsCandy} attempts remaining.`);
-    answerCandy = prompt(`Can you try another candy?`);
-    attemptsCandy--;
     }  
     
-  if (attemptsCandy === 0 && answerCandy !== johnnyCandy[i]){
-    alert(`My favorite candy are: ${johnnyCandy}`);
-    }
-  else if (answerCandy === johnnyCandy[i]){
-    alert('PHEW! You got one! Can you name another?');
-  } break;
-} 
+    if (attemptsCandy === 0 && answerCandy !== johnnyCandy[i]){
+        alert(`My favorite candy are: ${johnnyCandy}`);
+        }
+       break;
+    }  
 }
-
+}
 alert('Hope you learned some fun facts about me, ' + username);
-}
+alert(`You got ${correctguesses} out of 7 correct!`)
